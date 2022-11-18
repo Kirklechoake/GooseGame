b060f517ca51023ddc0a05f9a2fa876be5a54b07
Karan Sharma <kirklechoake666@gmail.com>
2022-11-17 22:27:41 -0500

    Create GooseGame.py

---
+++ GooseGame.py
@@ -0,0 +1,13 @@
+'''
+Start of the goose game
+We need a class for person, goose, and eggs
+Also need a function that controls the backround
+
+1. Start with initialization of the game and the backround
+2. Create an operating goose character
+3. Create the nest feature
+4. Create the person feature
+5. Bug testing
+
+Extra Menu screen, extra levels, difficulties
+'''
